881960572f30a352e2883d6180f24668ddd9e80c
m7md158 <m7md.a7md158@gmail.com>
2024-09-13 15:58:20 +0300

    last edit

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls', namespace='home')),
+    path('', include('home.urls', namespace='home')),
     path('about/', include('about.urls', namespace='about')),
     path('contact/', include('contact.urls', namespace='contact')),
     path('resume/', include('resume.urls', namespace='resume')),
